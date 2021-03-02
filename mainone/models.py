3e75b3ccb384649354c6bce9a8aa1596f79d9883
delaram <delaramfaetoot@gmail.com>
2021-03-02 16:20:10 +0330

    adding first module

--- mainone/models.py
+++ mainone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class test(models.Model):
+    number_of_the_exam=models.IntegerField()
+    exam_description = models.TextField()
+    dead_line=models.DateField()
+
+    def __str__(self):
+        return "test " + str(self.number_of_the_exam)
