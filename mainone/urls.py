3c5eee0872362b08c84dc3cf828de132e7db0c00
delaram <delaramfaetoot@gmail.com>
2021-03-04 20:39:33 +0330

    adding the login page still without specific permissions

--- mainone/urls.py
+++ mainone/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.login),
+    path('', views.login , name='login'),
+    path('logout/' , views.logingout, name="logout"),
     path('teacher/', views.teacher),
     path('student/', views.student),
     path('studentex/', views.studentexam),
--- mainone/views.py
+++ mainone/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate , login as auth_login , logout
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 def login(request):
+    if request.method == 'POST':
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        user=authenticate(request, username=username , password=password)
+        if user is not None:
+            auth_login(request , user)
+            videos=Videos.objects.all()
+            context={'videos':videos}
+            return render(request , 'mainone/teacher.html', context)
+        else:
+            messages.info(request , 'Username or password is wrong')
+
     return render(request, 'mainone/login.html')
 
+
+def logingout(request):
+    logout(request)
+    return redirect(login)
+
+@login_required(login_url='login')
 def teacher(request):
     videos=Videos.objects.all()
     context={'videos':videos}
@@ -18,7 +40,7 @@ def teachervid(request):
     return render(request, 'mainone/teachervid.html')
 
 
-
+@login_required(login_url='login')
 def student(request):
     videos=Videos.objects.all()
     context={'videos':videos}
@@ -30,7 +52,7 @@ def studentexam(request):
 def studentvid(request):
     return render(request, 'mainone/studentvid.html')
 
-
+@login_required(login_url='login')
 def video(request,pk):
     video=Videos.objects.get(id=pk)
     contexts={'video' : video}
